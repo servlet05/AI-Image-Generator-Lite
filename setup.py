10ce85a2cf92e04d826044141a5fdd1dc5198998
Servlet05 <adrian.d.servlet05@gmail.com>
2026-02-12 13:39:08 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ai-image-generator-lite",
+    version="0.1.0",
+    author="Tu Nombre",
+    description="Generador de imágenes IA ultra liviano para CPU",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tuusuario/ai-image-generator-lite",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "optimum-intel[openvino]>=1.14.0",
+        "diffusers>=0.24.0",
+        "pillow>=10.0.0",
+    ],
+)
